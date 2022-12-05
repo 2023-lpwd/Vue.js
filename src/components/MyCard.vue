44a3499269be4a22a9847b3c92fa81614aa4358d
Alex Chautemps <chautemps.alexandre@gmail.com>
2022-12-05 16:43:33 +0100

    initial commit

---
+++ src/components/MyCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    title: String,
+    srcImage: String,
+  },
+};
+</script>
+<template>
+  <div class="card">
+    <div class="card__image">
+      <img :src="srcImage" :alt="title" />
+    </div>
+    <p class="card__title">{{ title }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  max-width: 200px;
+  border-radius: 5px;
+  padding: 1rem;
+  -webkit-box-shadow: 5px 5px 16px -7px #dadada;
+  box-shadow: 5px 5px 16px -7px #dadada;
+}
+</style>
---
+++ src/components/MyCardList.vue
@@ -0,0 +1,51 @@
+<script>
+import MyCard from "./MyCard.vue";
+export default {
+  components: {
+    MyCard,
+  },
+  data() {
+    return {
+      playlists: [
+        {
+          id: 1,
+          title: "Playlist Détente",
+          srcImage: "http://placekitten.com/200/200",
+        },
+        {
+          id: 2,
+          title: "Mix années 90",
+          srcImage: "http://placekitten.com/200/200",
+        },
+        {
+          id: 3,
+          title: "Mix années 2000",
+          srcImage: "http://placekitten.com/200/200",
+        },
+        {
+          id: 4,
+          title: "Mix années 2020",
+          srcImage: "http://placekitten.com/200/200",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="list">
+    <MyCard
+      v-for="playlist in playlists"
+      :key="playlist.id"
+      :title="playlist.title"
+      :srcImage="playlist.srcImage"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.list {
+  display: flex;
+}
+</style>
